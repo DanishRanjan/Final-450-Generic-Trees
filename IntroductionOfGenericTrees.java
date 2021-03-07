5644b8b2041f3e66c6caf2ff075b3996383e9a48
Danish Ranjan <ranjandanish@gmail.com>
2021-03-07 19:07:21 +0530

    Introduction to Generic Tree

---
+++ IntroductionOfGenericTrees.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+
+public class IntroductionOfGenericTrees {
+
+	private class Node{
+		int data;
+		ArrayList<Node> children = new ArrayList<>();
+	}
+	
+	public static void main(String args[]) {
+		Node root;
+	}
+}
