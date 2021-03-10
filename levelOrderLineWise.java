13f4cb8ecd68f493bdaa94e5038d0bc47bcb1a19
Danish Ranjan <ranjandanish@gmail.com>
2021-03-11 01:37:35 +0530

    Multiple ways to print Level Order LineWise

--- levelOrderLineWise.java
+++ levelOrderLineWise.java
@@ -5,6 +5,14 @@ public class levelOrderLineWise {
   private static class Node {
     int data;
     ArrayList<Node> children = new ArrayList<>();
+    
+    Node(){
+    	
+    }
+    Node(int data){
+    	this.data = data;
+    }
+    
   }
 
   public static void display(Node node) {
@@ -91,13 +99,14 @@ public class levelOrderLineWise {
     System.out.println("Node Post " + node.data);
   }
 
-  public static void levelOrderLinewise(Node node){
+  public static void levelOrderLinewise1(Node node){
     Queue<Node> mainq = new ArrayDeque<>();
     mainq.add(node);
     Queue<Node> childq = new ArrayDeque<>();
     while(mainq.size()>0){
         node = mainq.remove();
         System.out.print(node.data+" ");
+        
         for(Node child: node.children){
             childq.add(child);
         }
@@ -106,11 +115,80 @@ public class levelOrderLineWise {
             childq = new ArrayDeque<>();
             System.out.println();
         }
-    }
-    
-    
+    } 
   }
-
+  
+//  ----------------Approach 2-----------------------Approach 2-------------------Approach 2----------------------------
+  public static void levelOrderLinewise2(Node node){
+	    Queue<Node> mainq = new ArrayDeque<>();
+	    mainq.add(node);
+	    mainq.add(new Node(-1));
+	    
+	    while(mainq.size()>0){
+	        node = mainq.remove();
+	        if(node.data != -1) {
+	        	System.out.print(node.data+" ");
+	        	
+		        for(Node child: node.children){
+		            mainq.add(child);
+		        }	
+	        }else {
+	        	if(mainq.size()>0) {
+	        		mainq.add(new Node(-1));
+	        		System.out.println();
+	        	}
+	        }  
+	    } 
+	  }
+//  -------------Approach 3(easiest)-----------Approach 3-----------Approach 3 -----------
+  public static void levelOrderLinewise3(Node node){
+	    Queue<Node> mainq = new ArrayDeque<>();
+	    mainq.add(node);
+	    
+	    while(mainq.size()>0){
+	    	//cicl = child in current level
+	         int cicl = mainq.size();
+	         for(int i=0;i<cicl;i++) {
+	        	 node = mainq.remove();
+	        	 System.out.print(node.data+" ");
+	        	 
+	        	 for(Node child: node.children) {
+	        		 mainq.add(child);
+	        	 }
+	         }
+	         System.out.println();
+	    }
+	  }
+  
+//-----------Approach 4-----------Approach 4--------------Approach 4-------  
+  private static class Pair{
+	  Node node;
+	  int level;
+	  
+	  Pair(Node node, int level){
+		  this.node = node;
+		  this.level = level;
+	  }
+  }
+  public static void levelOrderLinewise4(Node node){
+	    Queue<Pair> mainq = new ArrayDeque<>();
+	    mainq.add(new Pair(node, 1));
+	    int level = 1;
+	    while(mainq.size()>0) {
+	    	Pair p = mainq.remove();
+	    	if(p.level > level) {
+	    		level = p.level;
+	    		System.out.println();
+	    	}
+	    	System.out.println(p.node.data+" ");
+	    	for(Node child: p.node.children) {
+	    		Pair childPair = new Pair(child,p.level+1);
+	    		mainq.add(childPair);
+	    	}
+	    }
+	    
+	  }
+  
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -121,7 +199,10 @@ public class levelOrderLineWise {
     }
 
     Node root = construct(arr);
-    levelOrderLinewise(root);
+    levelOrderLinewise1(root);
+    levelOrderLinewise2(root);
+    levelOrderLinewise3(root);
+    levelOrderLinewise4(root);
   }
 
 }
